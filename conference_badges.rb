80fe03cfc9fc1176bbc7a3b8da2770e6ef98f86a
Grace Tan <gracetan37@gmail.com>
2019-06-05 14:07:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+
+def badge_maker(name)
+ return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+i = 0 
+badges = []
+while i< name.length 
+ badges.push("Hello, my name is #{name[i]}.")
+ i += 1 
+ end
+ return badges
+ end 
+ 
+ def assign_rooms(name)
+   name.each_with_index do |object,index|
+     indexone = index + 1 
+     puts "Hello #{object}! You'll be assigned to room #{indexone}!"
+   end
+  return assign_rooms
+ end
